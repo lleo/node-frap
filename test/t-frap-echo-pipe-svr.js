85d44a6c3f6f1fcb0384ded79c709f62fbdfb20a
LLeo <lleoem@gmail.com>
2012-05-31 02:27:47 -0400

    Stream implementation of the Frap echo via the Stream pipe API

---
+++ test/t-frap-echo-pipe-svr.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+var net = require('net')
+  , util = require('util')
+  , fs = require('fs')
+  , log = console.log
+  , format = util.format
+  , inspect = util.inspect
+  , Frap = require('frap').Frap
+  , repl = require('repl')
+  , nomnom = require('nomnom')
+
+var opts = nomnom.script('t-frap-echo-partial-svr')
+  .option('verbose', {
+    abbr: 'v'
+  , flag: true
+  , help: 'show more output'
+  })
+  .option('port', {
+    abbr: 'p'
+  , flag: false
+  , default: 7000
+  , help: 'listen port'
+  })
+  .parse()
+
+process.on('SIGINT', function () {
+  log('caught SIGINT')
+  process.exit(0)
+})
+
+//REALTIME MONITORING
+var root = { svrid : "localhost:"+opts.port }
+net.createServer(function(sk){
+  //spawned when another terminal `socat STDIN ./repl.sk`
+  // or better yet `socat READLINE ./repl.sk`
+  var replobj
+    , prompt = root.svrid+' > '
+    , myeval
+    , useGlobal=true
+    , ignoreUndefined
+  
+  replobj = repl.start(prompt, sk, myeval, useGlobal, ignoreUndefined)
+  replobj.context.root = root
+  
+//}).listen(7001)
+}).listen('./t-frap-echo-partial-svr.repl.sk')
+process.on('exit', function() {
+  fs.unlinkSync('./t-frap-echo-partial-svr.repl.sk')
+})
+
+var svr = {port: opts.port, verbose: opts.verbose}
+root.svr = svr
+
+svr.client = {}
+svr.sk = net.createServer()
+
+svr.sk.listen(svr.port)
+
+svr.sk.on('listening', function() {
+  log("listening to port "+svr.port)
+})
+
+svr.sk.on('connection', function(sk) {
+  var ident = sk.remoteAddress+":"+sk.remotePort
+  svr.client[ident] = {}
+  svr.client[ident].sk = sk
+  svr.client[ident].frap = new Frap(sk, true)
+
+  svr.client[ident].frap.on('frame', function(rstream, framelen){
+    var wstream = svr.client[ident].frap.createWriteStream(framelen)
+    rstream.pipe(wstream)
+  })
+
+  svr.client[ident].sk.on('end', function() {
+    svr.client[ident].sk.end()
+    log(ident+" disconnected")
+    delete svr.client[ident]
+  })
+})
+
+svr.sk.on('error', function(err) {
+  log("server sk error:", err)
+})
