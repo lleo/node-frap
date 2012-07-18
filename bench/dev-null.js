254f036ef1ed75096f45ac0b6d7d17b7a68ac680
LLeo <lleoem@gmail.com>
2012-07-18 05:48:54 -0400

    first

---
+++ bench/dev-null.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var net = require('net')
+  , nomnom = require('nomnom')
+  , log = console.log
+
+
+var opt = nomnom.script('raw-echo.js')
+  .option('port', {
+    abbr: 'p'
+  , flag: false
+  , default: 7000
+  , help: 'connect port; default 7000'
+  })
+  .parse()
+
+var svr = net.createServer(function(sk){
+  var id = sk.remoteAddress + ":" + sk.remotePort
+  log("connect %s", id)
+  sk.on('close', function(){ log("closed %s", id) })
+}).listen(opt.port)
+
+log("listening on %d", opt.port)
